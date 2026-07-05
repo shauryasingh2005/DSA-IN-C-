62b448fe6bab17c6b0481c46f13b93f377133c8b
shauryasingh2005 <shauryasingh2005@users.noreply.github.com>
2026-07-06 01:13:26 +0530

    Update c.cpp to output unique elements and count them

--- c.cpp
+++ c.cpp
@@ -1,35 +1,23 @@
 #include<iostream>
 using namespace std;
 int main(){
-  int n=11;
-  int arr[n]={1,2,0,1,0,0,0,1,2,2,1};
-  int co0=0,co1=0,co2=0;
-  for(int i=0;i<n;i++){
-    if(arr[i]==0){co0++;}
+  int n=9;
+  int a[n]={0,0,1,1,2,2,2,2,2};
+  
+  int j=0;
+  
+int count=1; 
+cout<<a[j];
+    while(j<n-1){
+       if(a[j]!=a[j+1]){
+      
+    cout<<a[j+1];
+    count++;
 
-  }for(int i=0;i<n;i++){
-    if(arr[i]==1){co1++;}
+    j++;
+          }
+  j++;}
+ 
 
-  }for(int i=0;i<n;i++){
-    if(arr[i]==2){co2++;}
-
-  }
-
-int i=0;
-  while(co0>0){
-    arr[i]=0;
-    cout<<arr[i];
-    i++;
-    co0--;
-  }
-while(co1>0){
-  arr[i]=1;
-    cout<<arr[i];
-    i++;
-    co1--;
-  }while(co2>0){
-    arr[i]=2;
-    cout<<arr[i];
-    i++;
-    co2--;
-  }}
+cout<<endl<<count<<endl;
+}
