04d0bda411119332cf32d2c3b2098237a02908b3
shauryasingh2005 <shauryasingh2005@users.noreply.github.com>
2026-07-17 03:38:59 +0530

    Add solution for longest substring with no repeated characters

--- LEETCODE.cpp
+++ LEETCODE.cpp
@@ -422,3 +422,35 @@ cout<<max;
 
 
 }
+
+
+
+
+
+//LONGEST SUBSTIRNG WHICH HAS NO REPEATED CHARACTERS
+#include<iostream>
+#include<climits>
+#include<unordered_map>
+using namespace std;
+int main(){
+
+string s = "abcbad";
+int l=0,h=0;
+int max=INT_MIN;
+unordered_map<char,int>f;
+while(h<5){
+f[s[h]]++;
+
+while(f.size()<(h-l+1)){
+    f[s[l]]--;
+    if(f[s[l]]==0){f.erase(s[l]);}
+    l++;}
+
+    if(f.size()==(h-l+1)){
+        if((h-l+1)>max){max=(h-l+1);}
+    }
+h++;
+
+}
+cout<<max;
+}
--- c.cpp
+++ c.cpp
@@ -1,34 +1,26 @@
-//FRUITS INTO BASKET{Longes tsubarray which has atmost 2 baskets for max numbe rof fruits,mtlb 1 ya 2 baskets possible h}
-
 #include<iostream>
-#include<unordered_map>
 #include<climits>
+#include<unordered_map>
 using namespace std;
 int main(){
+
+string s = "bbbbb";
+int l=0,h=0;
 int max=INT_MIN;
-int arr[5]={1,2,1,3,2};
-int k=2;/*as quesiton mein we have 2 baskets only*/
-int h=0,l=0;
-unordered_map<int,int>f;
+unordered_map<char,int>f;
 while(h<5){
-    f[arr[h]]++;
-    while(f.size()>k){
-        f[arr[l]]--;
-        if(f[arr[l]]==0){f.erase(arr[l]);}
-        l++;
+f[s[h]]++;
+
+while(f.size()<(h-l+1)){
+    f[s[l]]--;
+    if(f[s[l]]==0){f.erase(s[l]);}
+    l++;}
+
+    if(f.size()==(h-l+1)){
+        if((h-l+1)>max){max=(h-l+1);}
     }
+h++;
 
-/*MISTKAE    if(f.size()==k){  this would not be needed as exactly 2 nahi 2 ya 2 s ekm baskets me cover ho jaaye*/
-int len=h-l+1;
-if(len>max){max=len;}
-
-
-h++;}
-
-
-
+}
 cout<<max;
-
-
-
 }
